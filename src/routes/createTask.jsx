5b263fbab3a0fbc6e60d0155ec5b6ae44c03e632
Jim Blanchard <jablanch@amazon.com>
2022-11-10 18:01:35 -0600

    Fixed import & styles.

--- src/routes/createTask.jsx
+++ src/routes/createTask.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
 import { Text, View, Heading } from '@aws-amplify/ui-react';
 import { navigateHome } from '../utils';
-import { NewTaskForm } from '../ui-components';
+import { CreateTaskForm } from '../ui-components';
 
 function CreateTask() {
   const navigate = useNavigate();
@@ -11,7 +11,7 @@ function CreateTask() {
       <Heading level={3}>Amplified Task Planner</Heading>
       <Text marginBottom={"medium"} marginTop={"large"}>Use the form below to create a new task.</Text>
 
-      <NewTaskForm 
+      <CreateTaskForm 
         onSuccess={() => navigateHome(navigate)}
         onCancel={() => navigateHome(navigate)}
       />
